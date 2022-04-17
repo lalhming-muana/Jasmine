bdf0dfc42b14e2acebe612dc1f628b5a1c9d36a5
lhmuana <edd_vai@yahoo.co.in>
2022-04-17 16:14:27 +0530

    three stages of unit test completed

---
+++ Setting_Up/main.js
@@ -0,0 +1,14 @@
+
+// Three parts of testing
+// A typical flow is 
+// 1. You have a group.
+// 2. You have a test.
+// 2. You have an expectation.
+
+describe('Test Example',()=>{
+    it('returns true', ()=>{
+        expect(true).toBe(true);
+
+    });
+
+});
